be81a71782e2f2ebe1b5c6003e15b756b6c53805
Dante-Sal <dantecampuslands@gmail.com>
2025-04-22 02:29:57 +0000

    feat: :construction: Pokédex in progress

--- Dia3/js/main.js
+++ Dia3/js/main.js
@@ -1,45 +1,71 @@
 function Display(data) {
     let informacionHTML = document.getElementById('container__img');
-    if (data.response == "error") {
-        informacionHTML.innerHTML = `<p>Esto no funcionó :sadfeis:</p>`;
-    } else {
-        if (data["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"] == undefined) {
-            informacionHTML.innerHTML = `
+    if (data["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"] == undefined) {
+        informacionHTML.innerHTML = `
             <img src=${data["sprites"]["front_default"]} />
-            <p><span>${data["id"]} -</span> ${data["name"]}</p>
-            <form name="searchForm" id="searchForm" action="">
-                <input type="search" name="search" placeholder="Name or Number"/>
+            <p><span class="-${data["id"]}" id="identifier">${data["id"]}</span> <span>-</span> ${data["name"]}</p>
+            <form id="searchForm" name="searchForm">
+                <input name="search" type="search" placeholder="Name or Number"/>
+                <button id="prev" onclick="DisplayPrev()">Prev &lt</button>
+                <button id="next" onclick="DisplayNext()">Next &gt</button>
             </form>
             `;
-        } else {
-            informacionHTML.innerHTML = `
+    } else {
+        informacionHTML.innerHTML = `
             <img src=${data["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"]} />
-            <p><span>${data["id"]} -</span> ${data["name"]}</p>
-            <form name="searchForm" id="searchForm" action="">
-                <input type="search" name="search" placeholder="Name or Number"/>
+            <p><span class="-${data["id"]}" id="identifier">${data["id"]}</span> <span>-</span> ${data["name"]}</p>
+            <form id="searchForm" name="searchForm">
+                <input name="search" type="search" placeholder="Name or Number"/>
+                <button id="prev" onclick="DisplayPrev()">Prev &lt</button>
+                <button id="next" onclick="DisplayNext()">Next &gt</button>
             </form>
             `;
-        };
     };
     Fetch();
 };
 
 function Fetch() {
     const form = document.getElementById('searchForm')
-        form.addEventListener('submit', function(event) {
-            event.preventDefault();
-            let xhr = new XMLHttpRequest();
-            let formData = new FormData(form);
-            let link = `https://pokeapi.co/api/v2/pokemon/${formData.get('search')}`;
-            xhr.open('GET', link);
-            xhr.onreadystatechange = function () {
-                if (this.status == 200) {
-                    let respuesta = JSON.parse(this.responseText);
-                    Display(respuesta);
-                }
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        let xhr = new XMLHttpRequest();
+        let formData = new FormData(form);
+        let link = `https://pokeapi.co/api/v2/pokemon/${formData.get('search')}`;
+        xhr.open('GET', link);
+        xhr.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                let respuesta = JSON.parse(this.responseText);
+                Display(respuesta);
             };
-            xhr.send();
-        });
+        };
+        xhr.send();
+    });
+};
+
+function FetchPokemonById(id) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', `https://pokeapi.co/api/v2/pokemon/${id}`);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            let respuesta = JSON.parse(xhr.responseText);
+            Display(respuesta);
+        }
+    };
+    xhr.send();
+}
+
+function DisplayNext() {
+    const element = document.getElementById('identifier');
+    let protoID = element.getAttribute('class');
+    let id = parseInt(protoID.replace('-', ''));
+    FetchPokemonById(id + 1);
+};
+
+function DisplayPrev() {
+    const element = document.getElementById('identifier');
+    let protoID = element.getAttribute('class');
+    let id = parseInt(protoID.replace('-', ''));
+    FetchPokemonById(id - 1);
 };
 
 Fetch();
