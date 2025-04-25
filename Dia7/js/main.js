a3d688870f7e40bbcbd4d4115abba2866c9d623f
Dante-Sal1 <simondskolmi@gmail.com>
2025-04-25 02:31:17 +0000

    feat: :sparkles: (Dia7) Implementing winner definition and starting a new round

--- Dia7/js/main.js
+++ Dia7/js/main.js
@@ -1,3 +1,5 @@
+let deckId = '';
+let clickCounter = 0;
 let changeCounterPA = 0;
 let changeCounterPB = 0;
 let rotateFactor = 0;
@@ -15,19 +17,20 @@ function FETCH_NEW_DECK() {
     xhr.onreadystatechange = (function(){
         if (this.readyState === 4 && this.status === 200) {
             let response = JSON.parse(this.responseText);
-            FETCH_DECK(response);            
+            deckId = response['deck_id'];
+            FETCH_DECK(deckId);        
         };
     });
     xhr.send();
 };
 
-function FETCH_DECK(data) {
+function FETCH_DECK(id) {
     let xhr = new XMLHttpRequest;
-    xhr.open('GET', `https://deckofcardsapi.com/api/deck/${data['deck_id']}/draw/?count=2`);
+    xhr.open('GET', `https://deckofcardsapi.com/api/deck/${id}/draw/?count=2`);
     xhr.onreadystatechange = (function(){
         if (this.readyState === 4 && this.status === 200) {
             let response = JSON.parse(this.responseText);
-            DISPLAY_CARDS(response['cards']);        
+            DISPLAY_CARDS(response);        
         };
     });
     xhr.send();
@@ -39,24 +42,89 @@ function DISPLAY_CARDS(data) {
     const cardsPA = document.getElementById('subcontainer-A__card-container');
     const cardsPB = document.getElementById('subcontainer-B__card-container');
 
+    let cardPACode = data['cards'][0]['code'][0];
+    let cardPAValue = PARSE_CODE(cardPACode);
+    let cardPBCode = data['cards'][1]['code'][0];
+    let cardPBValue = PARSE_CODE(cardPBCode);
+
     button.addEventListener('click', (e) => {
+        clickCounter++;
 
-        button.innerHTML = 'Next round';
-        buttonPA.setAttribute('disabled', 'true');
-        buttonPB.setAttribute('disabled', 'true');
+        if (clickCounter % 2 === 1) {
+            button.innerHTML = 'Next round';
+            buttonPA.setAttribute('disabled', 'true');
+            buttonPB.setAttribute('disabled', 'true');
+    
+            cardsPA.innerHTML = `<img id="subcontainer-A__card" class="card" src="${data['cards'][0]['image']}"/>`;
+            const cardPA = document.getElementById('subcontainer-A__card');
+            cardPA.style.top = stylePA.top;
+            cardPA.style.left = stylePA.left;
+            cardPA.style.rotate = stylePA.rotate;
+    
+            cardsPB.innerHTML = `<img id="subcontainer-B__card" class="card" src="${data['cards'][1]['image']}"/>`;
+            const cardPB = document.getElementById('subcontainer-B__card');
+            cardPB.style.top = stylePB.top;
+            cardPB.style.left = stylePB.left;
+            cardPB.style.rotate = stylePB.rotate;
 
-        cardsPA.innerHTML = `<img id="subcontainer-A__card" class="card" src="${data[0]['image']}"/>`;
-        const cardPA = document.getElementById('subcontainer-A__card');
-        cardPA.style.top = stylePA.top;
-        cardPA.style.left = stylePA.left;
-        cardPA.style.rotate = stylePA.rotate;
+            if (cardPAValue > cardPBValue) {
+                let pileName = 'PA';
+                let xhr = new XMLHttpRequest;
+                xhr.open('GET', `https://deckofcardsapi.com/api/deck/${data['deck_id']}/pile/${pileName}/add/?cards=${data['cards'][0]['code']}`);
+                xhr.onreadystatechange = (function(){
+                    if (this.readyState === 4 && this.status === 200) {
+                        LIST_PILE(pileName);       
+                    };
+                });
+                xhr.send();
+            } else if (cardPAValue < cardPBValue) {
+                let pileName = 'PB';
+                let xhr = new XMLHttpRequest;
+                xhr.open('GET', `https://deckofcardsapi.com/api/deck/${data['deck_id']}/pile/${pileName}/add/?cards=${data['cards'][1]['code']}`);
+                xhr.onreadystatechange = (function(){
+                    if (this.readyState === 4 && this.status === 200) {
+                        LIST_PILE(pileName);       
+                    };
+                });
+                xhr.send();
+            };
+        } else {
+            button.innerHTML = 'Show cards';
+            buttonPA.removeAttribute('disabled');
+            buttonPB.removeAttribute('disabled');
 
-        cardsPB.innerHTML = `<img id="subcontainer-B__card" class="card" src="${data[1]['image']}"/>`;
-        const cardPB = document.getElementById('subcontainer-B__card');
-        cardPB.style.top = stylePB.top;
-        cardPB.style.left = stylePB.left;
-        cardPB.style.rotate = stylePB.rotate;
+            cardsPA.innerHTML = `<img id="subcontainer-A__cardReverse" class="cardReverse" src="https://deckofcardsapi.com/static/img/back.png"/>`;
+            cardsPB.innerHTML = `<img id="subcontainer-B__cardReverse" class="cardReverse" src="https://deckofcardsapi.com/static/img/back.png"/>`;
+
+            FETCH_DECK(deckId);
+        };
     });
+};
+
+function PARSE_CODE(code) {
+    switch (code) {
+        case 'J':
+            code = 11;
+        case 'Q':
+            code = 12;
+        case 'K':
+            code = 13;
+        case 'A':
+            code = 14;
+    };
+    return parseInt(code);
+};
+
+function LIST_PILE(pileName) {
+    let xhr = new XMLHttpRequest;
+    xhr.open('GET', `https://deckofcardsapi.com/api/deck/${data['deck_id']}/pile/${pileName}/list`);
+    xhr.onreadystatechange = (function(){
+        if (this.readyState === 4 && this.status === 200) {
+            let response = JSON.parse(this.responseText);
+            return response;
+        };
+    });
+    xhr.send();
 };
 
 FETCH_NEW_DECK();
