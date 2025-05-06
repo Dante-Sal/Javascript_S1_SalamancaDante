3307513392bd66296695f7d2dbe4452b56815f2d
Dante-Sal <dantecampuslands@gmail.com>
2025-05-06 09:11:10 -0500

    feat: :poop: (Dia12+1) script.js to refine

--- Dia12+1/script/script.js
+++ Dia12+1/script/script.js
@@ -1,43 +1,79 @@
-let data = new Object();
-
-const create_input = document.querySelector('.section-interface__form-input');
-const create_btn = document.querySelector('.section-interface__form-btn');
-const tasks_container = document.querySelector('.section-tasks');
-
-function FETCH_DATA() {
-    axios.get('https://6818a35a5a4b07b9d1d01dc7.mockapi.io/tasks')
-        .then(response => {
-            DISPLAY_DATA(response['data']);
-        });
-};
-
-function DISPLAY_DATA(data) {
-    for (let i = 0; i < data.length; i++) {
-        tasks_container.innerHTML += `
-        <div id="section-tasks__capsule--${data[i]['id']}" class="section-tasks__capsule">
-            <p id="section-tasks__capsule-text--${data[i]['id']}" class="section-tasks__capsule-text">${data[i]['task']}</p>
-            <div class="section-tasks__capsule-icon--checkmark-btn"></div>
-            <div class="section-tasks__capsule-icon--delete-btn"></div>
-        </div>`;
-        if (data[i]['status'] === 'ready') {
-            const capsule = document.getElementById(`section-tasks__capsule--${data[i]['id']}`);
-            const capsule_text = document.getElementById(`section-tasks__capsule-text--${data[i]['id']}`);
-
-            capsule.style.opacity = '50%';
-            capsule_text.style.textDecoration = 'line-through';
-        };
-    };
-};
-
 document.addEventListener('DOMContentLoaded', () => {
-    FETCH_DATA();
+    const create_input = document.querySelector('.section-interface__form-input');
+    const create_btn = document.querySelector('.section-interface__form-btn');
+    const tasks_container = document.querySelector('.section-tasks');
 
-    create_btn.addEventListener('click', () => {
-        tasks_container.innerHTML += `
-        <div class="section-tasks__capsule">
-            <p id="--${data.length + 1}" class="section-tasks__capsule-text">${create_input.value}</p>
-            <div class="section-tasks__capsule-icon--checkmark-btn"></div>
-            <div class="section-tasks__capsule-icon--delete-btn"></div>
+    async function FETCH_DATA(){
+        const res = await fetch('https://6818a35a5a4b07b9d1d01dc7.mockapi.io/tasks');//"{}" {}
+        data = await res.json();
+        return data;
+    }
+
+    async function FETCH_NEW(task) {
+        const req = {
+            'task': task,
+            'status': 'on_hold'
+        };
+        await axios.post('https://6818a35a5a4b07b9d1d01dc7.mockapi.io/tasks', req);
+        const data = await FETCH_DATA();
+        console.log(data);
+        DISPLAY_DATA(data);
+    };
+
+    function FETCH_COMPLETED(task, id) {
+        const req = {
+            'task': task,
+            'status': 'ready'
+        };
+        axios.put(`https://6818a35a5a4b07b9d1d01dc7.mockapi.io/tasks/${id}`, req);
+    };
+
+    function DISPLAY_DATA(data) {
+        tasks_container.innerHTML = '';
+        for (let i = 0; i < data.length; i++) {
+            tasks_container.innerHTML += `
+        <div id="section-tasks__capsule--${data[i]['id']}" data-id="${data[i]['id']}" class="section-tasks__capsule">
+            <p id="section-tasks__capsule-text--${data[i]['id']}" data-id="${data[i]['id']}" class="section-tasks__capsule-text">${data[i]['task']}</p>
+            <div id="section-tasks__capsule-icon--checkmark-btn--${data[i]['id']}" data-id="${data[i]['id']}" class="section-tasks__capsule-icon--checkmark-btn"></div>
+            <div id="section-tasks__capsule-icon--delete-btn--${data[i]['id']}" data-id="${data[i]['id']}" class="section-tasks__capsule-icon--delete-btn"></div>
         </div>`;
+            if (data[i]['status'] === 'ready') {
+                const capsule = document.getElementById(`section-tasks__capsule--${data[i]['id']}`);
+                const capsule_text = document.getElementById(`section-tasks__capsule-text--${data[i]['id']}`);
+                capsule.style.opacity = '50%';
+                capsule_text.style.textDecoration = 'line-through';
+            };
+        };
+        document.querySelectorAll('.section-tasks__capsule-icon--checkmark-btn').forEach(checkmark_btn => {
+            checkmark_btn.addEventListener('click', COMPLETE_TASKS);
+        });
+    };
+
+    function COMPLETE_TASKS(event) {
+        console.log(event)
+        const id = event.target.getAttribute('data-id');
+        console.log(id)
+        const task = document.getElementById(`section-tasks__capsule-text--${id}`).innerHTML;
+        const capsule_to_complete = document.getElementById(`section-tasks__capsule--${id}`);
+        const capsule_text_to_complete = document.getElementById(`section-tasks__capsule-text--${id}`);
+        FETCH_COMPLETED(task, id);
+        capsule_to_complete.style.opacity = '50%';
+        capsule_text_to_complete.style.textDecoration = 'line-through';
+    };
+
+    
+    create_btn.addEventListener('click', () => {
+        
+        if (create_input.value.trim() !== '') {
+            
+            FETCH_NEW(create_input.value);
+
+            
+        };
+        create_input.value = '';
+    });
+    FETCH_DATA().then(data=>{
+        console.log(data);
+        DISPLAY_DATA(data);
     });
 });
